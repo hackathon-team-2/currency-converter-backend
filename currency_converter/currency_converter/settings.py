e028db7a450d77eedbe009c8b41d83602fb80a1e
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-12-23 16:04:34 +0500

    #12: feat: Install + settings + urls

--- currency_converter/currency_converter/settings.py
+++ currency_converter/currency_converter/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'users',
     'api',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -90,3 +91,7 @@ STATIC_URL = 'static/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
