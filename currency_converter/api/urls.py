2fa30cbd7f10c15a8d3d72c9a1b2149ade94937f
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-12-25 11:21:38 +0400

    #18 feat/moved schema/ and convert/ here from root urls

--- currency_converter/api/urls.py
+++ currency_converter/api/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
+from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
+                                   SpectacularSwaggerView)
 
 from api import views
 
 urlpatterns = [
-    path('', views.CurrencyView.as_view()),
+    path('convert/', views.CurrencyView.as_view()),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path(
+        'schema/swagger-ui/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui'),
+    path(
+        'schema/redoc/',
+        SpectacularRedocView.as_view(url_name='schema'),
+        name='redoc'),
 ]
