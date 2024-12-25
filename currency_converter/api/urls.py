40951be1f790395a4921cf7fe0914f848a28b5c8
Татьяна Шарова <tanyasharova2001@mail.ru>
2024-12-25 16:41:42 +0400

    #18 feat/change urls

--- currency_converter/api/urls.py
+++ currency_converter/api/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
-                                   SpectacularSwaggerView)
 
 from api import views
 
@@ -8,13 +6,4 @@ app_name = 'api'
 
 urlpatterns = [
     path('convert/', views.CurrencyView.as_view(), name='convert'),
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path(
-        'schema/swagger-ui/',
-        SpectacularSwaggerView.as_view(url_name='schema'),
-        name='swagger-ui'),
-    path(
-        'schema/redoc/',
-        SpectacularRedocView.as_view(url_name='schema'),
-        name='redoc'),
 ]
